be51c0a4508b862e6eebe2e52b064afa54eb7609
Jesús Avelar <wisusdev@gmail.com>
2024-07-30 15:50:22 -0600

    feat: Add API endpoints for managing providers in the API

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,10 +3,15 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Auth\AuthenticationException;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
@@ -14,5 +19,20 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+
+        $exceptions->renderable(function (NotFoundHttpException $e) {
+			throw new App\Exceptions\JsonApi\NotFoundHttpException;
+        });
+
+        $exceptions->renderable(function (BadRequestHttpException $e) {
+            throw new App\Exceptions\JsonApi\BadRequestHttpException($e->getMessage());
+        });
+
+        $exceptions->renderable(function (AuthenticationException $e) {
+            throw new App\Exceptions\JsonApi\AuthenticationException;
+        });
+
+        $exceptions->renderable(function (AccessDeniedHttpException $e) {
+            throw new App\Exceptions\JsonApi\UnauthorizedException;
+        });
     })->create();
